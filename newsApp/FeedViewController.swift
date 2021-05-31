e3a5234ea8a66aff28d9098e174695b98d5f2e0d
Alperen Kişi <alperenkisi@gmail.com>
2021-05-31 12:22:50 +0300

    initial commit

---
+++ newsApp/FeedViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  newsApp
+//
+//  Created by Alperen Kişi on 31/05/2021.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    //MARK: -UI Objects -
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
